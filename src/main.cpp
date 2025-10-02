e87285896ef20a5d5127a8601057d88b80690938
Nicholas Seifert <nicholasseifert81@gmail.com>
2025-10-02 16:42:33 -0300

    teste led azul
    Tudo OK!

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(2, OUTPUT);  // LED_BUILTIN em muitos ESP32 DOIT
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(2, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  delay(500);
 }
